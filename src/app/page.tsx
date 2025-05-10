a2fc224ccfe5bbf37b77614153c4b85453a124a5
SonuR12 <sonurai7011@gmail.com>
2025-05-10 10:28:39 +0530

    resnd add

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import ContactForm from "@/components/shared/contact-form";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+    <ContactForm />
+    </div>
+  );
+}
---
+++ src/components/ui/success.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import Image from "next/image"
+
+interface SuccessModalProps {
+  open: boolean;
+  onClose: () => void;
+  data: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    contactNo: string;
+    message: string;
+  };
+}
+
+export default function SuccessModal({ open, onClose}: SuccessModalProps) {
+  return (
+    <Dialog open={open} onOpenChange={onClose}>
+      <DialogContent className="text-center flex flex-col items-center justify-center gap-2 bg-white">
+        <DialogHeader>
+          <div className="flex justify-center">
+            <Image height={200} width={200} src='/images/success.jpg' alt="success" />
+          </div>
+          <DialogTitle className="mt-2 text-xl font-semibold">
+            Thank you for reaching out to us!
+          </DialogTitle>
+        </DialogHeader>
+
+        <p className="text-muted-foreground text-sm text-center">
+          We&apos;ve received your message and will respond shortly 😊
+        </p>
+      </DialogContent>
+    </Dialog>
+  )
+}
